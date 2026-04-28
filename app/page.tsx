29a744de20e1fea60c34fc1f44b0cfdcbf2037ec
kopasovska <v.kopasovska@gmail.com>
2026-04-28 13:46:04 +0200

    Chore: Setup firebase realtime database

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
+import { db } from "@/lib/firebase";
+import { onValue, ref } from "firebase/database";
+
+export default async function Home() {
+  // onValue(ref(db, "nannies"), (snapshot) => {
+  //   const data = snapshot.val();
+  //   console.log(data);
+  // });
   return <></>;
 }
