55adc8ed33769b9468228e4e586d6a032ca12981
kopasovska <v.kopasovska@gmail.com>
2026-04-29 19:19:30 +0200

    Feat: header layout for desktop/tablets + custom color tokens

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,22 +1,30 @@
+import NavLink from "./NavLink";
+
 const Header = () => {
-  return (
-    <header className="h-[88px] mx-h-88 flex px-[128px] py-[20px] bg-(--deep-green) border border-(--black) text-(--white)">
-      <p className="text-2xl font-medium leading-[117%] tracking-[0.02em]">
-        Nanny.Services
-      </p>
-      <nav>
-        <ul className="flex">
-          <li>Home</li>
-          <li>Nannies</li>
-          <li>Favorites</li>
-        </ul>
-      </nav>
-      <div className="flex">
-        <div>Avatar</div>
-        <button>Log out</button>
-      </div>
-    </header>
-  );
+    return (
+        <header className="hidden md:flex justify-between items-center h-22 px-32 py-5 bg-primary border border-foreground text-secondary">
+            <p className="text-2xl font-medium leading-[117%] tracking-[0.02em]">
+                Nanny.Services
+            </p>
+            <nav>
+                <ul className="flex gap-10 text-body-sm">
+                    <li>
+                        <NavLink href="/">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink href="/nannies">Nannies</NavLink>
+                    </li>
+                    <li>
+                        <NavLink href="/favorites">Favorites</NavLink>
+                    </li>
+                </ul>
+            </nav>
+            <div className="flex gap-6">
+                <div>Avatar</div>
+                <button>Log out</button>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
---
+++ components/layout/NavLink.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+interface NavLinkProps {
+    href: string;
+    children: React.ReactNode;
+}
+const NavLink = ({ href, children }: NavLinkProps) => {
+    const pathname = usePathname();
+    const isAtive = pathname === href;
+    return (
+        <Link
+            href={href}
+            className={
+                isAtive
+                    ? "relative after:content-[''] after:absolute after:-bottom-3 after:left-1/2 after:h-2 after:w-2 after:-translate-x-1/2 after:rounded-full after:bg-secondary"
+                    : ""
+            }
+        >
+            {children}
+        </Link>
+    );
+};
+
+export default NavLink;
